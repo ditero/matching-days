77411e07a2b733097baa03777b9dda2cb6db284f
ditero <anelesigenu@gmail.com>
2017-03-20 12:06:04 +0200

    Matching days commit to github

---
+++ matching_days.js
@@ -0,0 +1,44 @@
+//Author: Anele Sigenu
+//Excercise: Mathcing days
+//Year 2017 @ ProjectCodex
+//Declare and selects Elemnts from the HTML index file
+
+var listDays = document.querySelectorAll('td');
+var btnCmp = document.getElementById('compBtn');
+var thisDay = document.getElementById('firstDate');
+var thatDay = document.getElementById('secDate');
+
+//Match Days
+var dayM = function matchDays(){
+
+  var dayOne = new Date(thisDay.value);
+  var dayTwo = new Date(thatDay.value);
+
+     var stDay = dayOne.getDay();
+     var ndDay = dayTwo.getDay();
+
+     reFresh();
+     //If days match Shade them with GREEN Color.
+      if(stDay === ndDay){
+
+          console.log(listDays[stDay]);
+           listDays[stDay].className = 'paintDay';
+      }
+      // else days do'nt match shade them with different Colors Red and Blue.
+      else{
+
+         listDays[stDay].className = 'paintDayRed';
+         listDays[ndDay].className = 'paintDayBlue';
+
+      }
+};
+//Reset the table i.e  the unshade all the columns.
+function reFresh(){
+  for (var i = 0; i < listDays.length; i++) {
+    listDays[i].classList.remove('paintDay');
+    listDays[i].classList.remove('paintDayRed');
+    listDays[i].classList.remove('paintDayBlue');
+  }
+};
+//Add Event Listener to the Button to trigger the processs.
+btnCmp.addEventListener('click',dayM);
